7ebb8f67a98e4f6e9d70c14f2bbdb47be9cc3743
rainyPoseidon <laws_basic@yahoo.co.uk>
2024-01-03 23:07:01 +0800

    Configuring JWT service.

--- src/main/java/com/example/springjwtdemo/service/AuthService.java
+++ src/main/java/com/example/springjwtdemo/service/AuthService.java
@@ -14,13 +14,18 @@ public class AuthService {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationResponse register(RegisterRequest request){
+    private final JWTService jwtService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder().firstname(request.getFirstname())
                 .lastname(request.getLastname())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .build();
         userService.saveUser(user);
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
 
     }
 }
